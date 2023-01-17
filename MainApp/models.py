b8d41ef2f694a012407ba3dbd6060c312f95832e
gepolis <120267018+gepolis@users.noreply.github.com>
2023-01-17 21:46:14 +0300

    Add files via upload

--- MainApp/models.py
+++ MainApp/models.py
@@ -5,3 +5,6 @@ class Item(models.Model):
     name = models.CharField(max_length=100)
     brand = models.CharField(max_length=100)
     count = models.PositiveIntegerField()
+    desc = models.CharField(max_length=255)
+    cost = models.PositiveIntegerField(default=1000)
+    imgurl = models.CharField(max_length=255)
--- MainApp/views.py
+++ MainApp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, HttpResponse, Http404
-
+from MainApp.models import Item
 def home(request):
     # text = f""" <h1>"Изучаем django"</h1> <strong>Автор</strong>: <i>Лазуренко Н.С.</i>"""
     # return HttpResponse(text)
@@ -8,13 +8,6 @@ def home(request):
 def about(request):
     return render(request,"about.html")
 
-items = [
-   {"id": 1, "name": "Кроссовки abibas" ,"quantity":5},
-   {"id": 2, "name": "Куртка кожаная" ,"quantity":2},
-   {"id": 3, "name": "Coca-cola 1 литр" ,"quantity":12},
-   {"id": 4, "name": "Картофель фри" ,"quantity":0},
-   {"id": 5, "name": "Кепка" ,"quantity":124},
-]
 def items_list(request):
     # items_result = "<ol>"
     # for item in items:
@@ -22,19 +15,22 @@ def items_list(request):
     # items_result += "</ol>"
     # return HttpResponse(items_result)
     context = {
-        "items": items
+        "items": Item.objects.all()
     }
     return render(request,"items.html",context)
 
 
 
 
-
 def item_page(request,id):
-    for item in items:
-        if item['id'] == id:
-    #         item_str = f"товар {item['name']} количество{item['quantity']}"
-    #         return HttpResponse(item_str)
-    # return HttpResponse(f"Товар с id = {id} не найден")
-            return render(request,"item_page.html", item)
-    raise Http404(f"Товар с id = {id} не найден")
+    item = Item.objects.get(pk=id)
+    # items_result = "<ol>"
+    # for item in items:
+    #     items_result += "<li>" + f"<a href ='/item/{item['id']}'> " + item['name'] + "</a>" + "</li>"
+    # items_result += "</ol>"
+    # return HttpResponse(items_result)
+    context = {
+        "item": item
+    }
+    return render(request, "item_page.html", context)
+
